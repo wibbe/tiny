15e11404ae80335869fe7e2cef970d2bca0f2a74
Daniel Wiberg <daniel@skillster.se>
2017-08-12 14:43:45 +0200

    Moved implementation into one single file.

--- source/app.d
+++ source/app.d
@@ -2,6 +2,20 @@ import std.stdio;
 import tiny;
 
 
+class TestApp : IApplication {
+	static IApplication create(Context ctx) {
+		return new TestApp();
+	}
+
+	bool step(Context ctx) {
+		return true;
+	}
+
+	void paint(IPainter painter) {
+
+	}
+}
+
 void main()
 {
 	auto r = Rect(0, 0, 100, 100);
@@ -14,4 +28,6 @@ void main()
 	writeln("Dot: ", dot(Vec2(1f, 0f), Vec2(0.5f, 1f)));
 
 	writeln("It runs! ", r.top);
+
+	run!TestApp("Test App", 320, 240, 2);
 }
