ac58c4b4ba27b9b8325eff45152170b1ee10ec82
Daniel Wiberg <daniel@skillster.se>
2017-08-14 09:54:32 +0200

    Revert "Moved stuff out to their own files."
    This reverts commit c731e777118589e5a8d56942f67fded1aeac68e7.

--- source/app.d
+++ source/app.d
@@ -1,43 +1,34 @@
-
 import std.stdio;
-import std.datetime.stopwatch : StopWatch, AutoStart;
-import core.time : Duration;
-import core.thread : Thread, nsecs;
+import tiny;
 
-import platform;
-import math;
-import bitmap;
 
-enum WIDTH = 320;
-enum HEIGHT = 200;
-enum SCALE = 3;
-enum TARGET_FRAME_TIME = nsecs(33333333);
+class TestApp : IApplication {
+
+	this() {
+	}
+
+	bool step() {
+		return true;
+	}
+
+	void paint(IPainter painter) {
+		painter.clear(COLOR_WHITE);
+		painter.pixel(10, 10, COLOR_BLACK);
+		painter.pixel(painter.width - 10, painter.height - 10, COLOR_BLACK);
+	}
+}
+
+extern (C) int setup() {
+	writeln("Setup!");
+	return 0;
+}
+
+extern (C) void paint(IPainter painter) {
+	painter.clear(COLOR_WHITE);
+	painter.pixel(10, 10, COLOR_BLACK);
+	painter.pixel(painter.width - 10, painter.height - 10, COLOR_BLACK);
+}
 
 void main() {
-	IWindow win = createWindow("Tiny RTS", WIDTH * SCALE, HEIGHT * SCALE);
-
-   Bitmap canvas = new Bitmap(320, 200);
-   BitmapPainter painter = new BitmapPainter(canvas);
-
-   auto frameWatch = new StopWatch(AutoStart.yes);
-
-   win.show();
-
-   while (true) {
-      frameWatch.reset();
-
-      if (!win.pump())
-         break;
-
-      painter.clear(Color(255, 255, 0));
-      win.paint(canvas);
-
-      auto frameDuration = frameWatch.peek();
-      //_frameTime = cast(double)frameDuration.split!("nsecs").nsecs / 1_000_000_000.0;
-
-      if (frameDuration < TARGET_FRAME_TIME) {
-         auto sleepTime = TARGET_FRAME_TIME - frameDuration;
-         Thread.sleep(sleepTime);
-      }
-   }
+	run!TestApp("Test App", 320, 240, 2);
 }
